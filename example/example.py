43b4ec44409bb7e928a1ab461f9963d4ad03b26c
Ken <krisken4699@gmail.com>
2022-10-16 03:43:41 -0600

    Updated example.py

--- example/example.py
+++ example/example.py
@@ -1,6 +1,12 @@
+
 import sys
 import os
 # sys.path.insert(0, "../")
-sys.path.append("../")
+# sys.path.append("../")
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import ken_api
-print(ken_api.apiV2.get_assets('SXNTF', "SWSSF", simple=True))
+print(ken_api.get_assets('SXNTF', "SWSSF", "MANR", simple=True))
+print(ken_api.get_assets('PCSGF', simple=True))
+print(ken_api.get_assets('PCSGF'))
+
+ken_api.download_symbols()
--- ken_api.py
+++ ken_api.py
@@ -1,95 +1,97 @@
+
 from pydoc import describe
 import requests
 import json
 # from typing import Union
 import os
-
-# dir_path = os.path.dirname(os.path.abspath(os.getcwd()))
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 global APCA_API_KEY_ID
 global APCA_API_SECRET_KEY
 APCA_API_KEY_ID = ""
 APCA_API_SECRET_KEY = ""
-# API_DOMAIN = "https://api.alpaca.markets"
-API_DOMAIN = "https://paper-api.alpaca.markets"
 
+
+# dir_path = os.path.dirname(os.path.abspath(os.getcwd()))
+dir_path = os.path.dirname(os.path.realpath(__file__))
 with open(dir_path + '/alpaca_credentials.json') as credentials:
     data = json.load(credentials)
     APCA_API_KEY_ID = data["APCA-API-KEY-ID"]
     APCA_API_SECRET_KEY = data["APCA-API-SECRET-KEY"]
 
-def APCA_request(path, headers={
+
+def __APCA_request(path, headers={
     'APCA-API-KEY-ID': APCA_API_KEY_ID,
     'APCA-API-SECRET-KEY': APCA_API_SECRET_KEY,
 }):
     return requests.get(path, headers=headers)
 
-class apiV2:
-        
 
-    def download_symbols(path: str = "./") -> dict | int:
-        """
-        downloads a all symbols to ./symbols_list in the same directory, symbols_list.\n
-        \t:param str path: Path to the download, default directory is module directory.
-        """
-        res = APCA_request(API_DOMAIN + '/v2/assets')
+# API_DOMAIN = "https://api.alpaca.markets"
+API_DOMAIN = "https://paper-api.alpaca.markets"
+
+
+def download_symbols(path: str = "./") -> dict | int:
+    """
+    downloads a all symbols to ./symbols_list in the same directory, symbols_list.\n
+    \t:param str path: Path to the download, default directory is module directory.
+    """
+    res = __APCA_request(API_DOMAIN + '/v2/assets')
+    if res.status_code == 404:
+        return 404
+    if res.status_code != 200:
+        return res.status_code
+    res = res.json()
+    symbol_list = []
+    if(path == ""):
+        path = + "."
+    if path[len(path)-1] != "/" and path[len(path)-1] != "\\":
+        path += "/"
+    with open(path + "symbols_list", "w", encoding="utf-8") as outfile:
+        for asset in res:
+            symbol_list.append(str(
+                {
+                    "symbol": asset["symbol"],
+                    "name": asset["name"],
+                    "class": asset['class'],
+                    "id": asset["id"],
+                    "status": asset['status'],
+                }))
+        symbol_list = "\n".join((symbol_list))
+        outfile.write(symbol_list)
+    return symbol_list
+
+
+def get_assets(*symbols: str, **simple: bool) -> dict | int:
+    """
+    Returns a simple dict including symbol, name, class, id, and status\n
+    \t:args str symbols: symbols to request for assets\n
+    \t:optional bool simple: returns a simple format  
+    """
+    simple_ = False
+
+    for key, value in simple.items():
+        if(key == "simple" and value == True):
+            simple_ = True
+    symbol_list = []
+    if symbols == ():
+        symbols = [""]
+    for symbol in symbols:
+        res = __APCA_request(API_DOMAIN + '/v2/assets/' + str(symbol))
         if res.status_code == 404:
             return 404
         if res.status_code != 200:
             return res.status_code
         res = res.json()
-        symbol_list = []
-        if(path == ""):
-            path = + "."
-        if path[len(path)-1] != "/" and path[len(path)-1] != "\\":
-            path += "/"
-        with open(path + "symbols_list", "w", encoding="utf-8") as outfile:
-            for asset in res:
-                symbol_list.append(str(
-                    {
-                        "symbol": asset["symbol"],
-                        "name": asset["name"],
-                        "class": asset['class'],
-                        "id": asset["id"],
-                        "status": asset['status'],
-                    }))
-            symbol_list = "\n".join((symbol_list))
-            outfile.write(symbol_list)
-        return symbol_list
+        if simple_:
+            symbol_list.append(str(
+                {
+                    "symbol": res["symbol"],
+                    "name": res["name"],
+                    "class": res['class'],
+                    "id": res["id"],
+                    "status": res['status'],
+                }))
+        else:
+            symbol_list.append(str(res))
+    symbol_list = "\n".join((symbol_list))
 
-    def get_assets(*symbols: str, **simple: bool) -> dict | int:
-        """
-        Returns a simple dict including symbol, name, class, id, and status\n
-        \t:args str symbols: symbols to request for assets\n
-        \t:optional bool simple: returns a simple format  
-        """
-        simple_ = False
-
-        for key, value in simple.items():
-            if(key == "simple" and value == True):
-                simple_ = True
-        symbol_list = []
-        if symbols == ():
-            symbols = [""]
-        for symbol in symbols:
-            res = APCA_request(API_DOMAIN + '/v2/assets/' + str(symbol))
-            if res.status_code == 404:
-                return 404
-            if res.status_code != 200:
-                return res.status_code
-            res = res.json()
-            if simple_:
-                symbol_list.append(str(
-                    {
-                        "symbol": res["symbol"],
-                        "name": res["name"],
-                        "class": res['class'],
-                        "id": res["id"],
-                        "status": res['status'],
-                    }))
-            else:
-                symbol_list.append(str(res))
-        symbol_list = "\n".join((symbol_list))
-
-        return symbol_list
+    return symbol_list
