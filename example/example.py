87a87a240d2a89a51cdc0a7690c578cbd46d532f
Ken <krisken4699@gmail.com>
2022-10-17 23:57:24 -0600

    Made ken_api a lib. Install with: pip install tbc-ken-api

--- example/example.py
+++ example/example.py
@@ -1,12 +1,11 @@
 
 import sys
 import os
-# sys.path.insert(0, "../")
-# sys.path.append("../")
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import ken_api
-print(ken_api.get_assets('SXNTF', "SWSSF", "MANR", simple=True))
-print(ken_api.get_assets('PCSGF', simple=True))
-print(ken_api.get_assets('PCSGF'))
-print(ken_api.get_bars("TSLA", start="2022-10-05", end="2022-10-15"))
-ken_api.download_symbols()
+api = ken_api.api()
+print(api.get_assets('SXNTF', "SWSSF", "MANR", simple=True))
+print(api.get_assets('PCSGF', simple=True))
+print(api.get_assets('PCSGF'))
+print(api.get_bars("TSLA", start="2022-10-05", end="2022-10-15"))
+api.download_symbols()
+print(api)
