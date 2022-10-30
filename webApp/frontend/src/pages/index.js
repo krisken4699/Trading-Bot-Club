2dbd18621b903400f1581702d503032456b1645b
Ken <krisken4699@gmail.com>
2022-10-29 22:39:51 -0600

    Chaneged dir tree

---
+++ webApp/frontend/src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
