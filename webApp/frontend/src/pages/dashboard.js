c4e38982d977b129ef1f1fe1d780dc25d31ed928
Ken <krisken4699@gmail.com>
2022-11-06 22:09:31 -0700

    Remake landing page and optimized router

--- webApp/frontend/src/pages/dashboard.js
+++ webApp/frontend/src/pages/dashboard.js
@@ -1,13 +1,35 @@
+import gsap from "gsap"
 import React, { useState, useEffect, useRef } from "react"
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 const Dashboard = () => {
+    const main = useRef(null)
 
+    gsap.registerPlugin(ScrollTrigger)
+    useEffect(() => {
+        let ctx = gsap.context(() => {
+            ScrollTrigger.create({
+                trigger: "#test",
+                start: 'top top',
+                end: "+=100%",
+                pin: '#test',
+                markers: true
+            })
 
+            return () => ctx.revert(); // cleanup
+        }, main); // <- IMPORTANT! Scopes selector text
+
+    }, []);
 
     return (
-        <div style={{scrollSnapType:"y mandatory", msScrollSnapPointsY:"repeat(100vh)"}} className="h-[200vh] !overflow-y-scroll snap-y snap-always flex snap-mandatory">
-            <div style={{scrollSnapAlign:"center", position:'relative'}} className="snap-start h-[100vh] bg-red-400">afds</div>
-            <div style={{scrollSnapAlign:"center", position:'relative'}} className="snap-start h-[100vh] bg-green-500">sfd</div>
+        <div ref={main} className="h-[200vh]">
+            <div className="w-screen h-[100vh] bg-red-400">
+            </div>
+            <div className="w-screen h-[100vh] bg-green-500">
+                <h1 className="text-5xl h-screen text-center" id="test">adno</h1>
+
+            </div>
+            <div className="w-screen h-[100vh] bg-blue-500">sfd</div>
         </div>
     )
 }
